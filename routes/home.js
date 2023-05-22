9826bbd2e33b06c50ce9ba5d2f96013357c438ba
roger <rogerferdian@gmail.com>
2023-05-22 22:14:40 +0700

    fixing comment by git

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,8 @@ const express = require("express")
 const homeRouter = express.Router()
 const checkLogin = require("../helper/check_login")
 
-<<<<<<< Updated upstream
-homeRouter.get("/", (req, res) => {
-    res.render("index")
-=======
 homeRouter.get("/", async (req, res) => {
     if(req.session.loggedin) res.render("index")
     else res.render("index-guest")
->>>>>>> Stashed changes
 })
 module.exports = homeRouter
